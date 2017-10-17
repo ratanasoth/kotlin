87f7db57ebc1a0925203a1a5abbd27d8a2463433
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-10-17 17:26:37 +0300

    Test completion in script files
    Test for 'JavaResolutionUtils.getJavaDescriptorResolver' failing with AE
    Part of  EA-105435

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -136,6 +136,7 @@ import org.jetbrains.kotlin.idea.refactoring.safeDelete.AbstractMultiModuleSafeD
 import org.jetbrains.kotlin.idea.refactoring.safeDelete.AbstractSafeDeleteTest
 import org.jetbrains.kotlin.idea.repl.AbstractIdeReplCompletionTest
 import org.jetbrains.kotlin.idea.resolve.*
+import org.jetbrains.kotlin.idea.script.AbstractScriptConfigurationCompletionTest
 import org.jetbrains.kotlin.idea.script.AbstractScriptConfigurationHighlightingTest
 import org.jetbrains.kotlin.idea.script.AbstractScriptConfigurationNavigationTest
 import org.jetbrains.kotlin.idea.slicer.AbstractSlicerTest
@@ -1112,6 +1113,10 @@ fun main(args: Array<String>) {
 
         testClass<AbstractScriptConfigurationNavigationTest> {
             model("script/definition/navigation", extension = null, recursive = false)
+        }
+
+        testClass<AbstractScriptConfigurationCompletionTest> {
+            model("script/definition/completion", extension = null, recursive = false)
         }
 
         testClass<AbstractNameSuggestionProviderTest> {
